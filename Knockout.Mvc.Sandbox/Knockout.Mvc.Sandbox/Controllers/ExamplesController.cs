c7b64fe61d9315679cefca997c3dda2c7eb77c54
Lukasz Lysik <llysik@gmail.com>
2013-01-06 21:56:05 +0100

    Initial

---
+++ Knockout.Mvc.Sandbox/Knockout.Mvc.Sandbox/Controllers/ExamplesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Knockout.Mvc.Sandbox.Models;
+
+namespace Knockout.Mvc.Sandbox.Controllers
+{
+    public class ExamplesController : Controller
+    {
+        public ActionResult Index()
+        {
+            var viewModel = new ExamplesIndexViewModel
+            {
+                Requirements = new List<AdditionalRequirement>
+                {
+                    new AdditionalRequirement { Id = 7, Description = "Kto?"},
+                    new AdditionalRequirement { Id = 12, Description = "Gdzie?"},
+                    new AdditionalRequirement { Id = 14, Description = "Kiedy?"},
+                    new AdditionalRequirement { Id = 15, Description = "Ile?"}
+                }
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ Knockout.Mvc.Sandbox/Knockout.Mvc.Sandbox/Models/ExamplesIndexViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Knockout.Mvc.Sandbox.Models
+{
+    public class ExamplesIndexViewModel
+    {
+        public IList<AdditionalRequirement> Requirements { get; set; }
+    }
+
+    public class AdditionalRequirement
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+    }
+}
